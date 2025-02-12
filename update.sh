e815e82163417564c828f828abc47269dd2de91e
EnergoStalin <ens.stalin@gmail.com>
2025-02-13 02:22:39 +0300

    build(update): manually update allCallTimers from remote in script

--- update.sh
+++ update.sh
@@ -1,14 +1,22 @@
-git checkout dev
+git switch dev
 gh repo sync $(git ls-remote --get-url origin) -b dev
 git pull
 
-git checkout roleColorEverywhere
+git switch roleColorEverywhere
 git rebase dev
 git push -f
 
-git checkout local
+git switch local
 git rebase roleColorEverywhere
 git push -f
+
+tmp=`mktemp -d`
+git clone -b fix-vencord-patch-locations git@github.com:Max-Herbold/AllCallTimersDiscordPlugin.git $tmp
+mv -f $tmp/allCallTimers/*.tsx -t src/plugins/allCallTimers
+rm -rf $tmp
+
+git add src/plugins/allCallTimers
+git commit -m 'chore(allCallTimers): auto upstream update'
 
 pnpm install
 pnpm build
